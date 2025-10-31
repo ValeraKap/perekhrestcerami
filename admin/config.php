1086a40a0032b8cda60c00f4e125adb8a408aa0d
Valerii Kapitanchuk <valkozur.e@gmail.com>
2025-10-31 23:32:13 +0200

    Clean site - 311025

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://perekhrest-ceramic.com/admin/');
+define('HTTP_CATALOG', 'http://perekhrest-ceramic.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://perekhrest-ceramic.com/admin/');
+define('HTTPS_CATALOG', 'https://perekhrest-ceramic.com/');
+
+// DIR
+define('DIR_APPLICATION', '/home/perekhrestcerami/public_html/admin/');
+define('DIR_SYSTEM', '/home/perekhrestcerami/public_html/system/');
+define('DIR_IMAGE', '/home/perekhrestcerami/public_html/image/');
+define('DIR_STORAGE', '/home/perekhrestcerami/storage/');
+define('DIR_CATALOG', '/home/perekhrestcerami/public_html/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'perekhrestcerami_db');
+define('DB_PASSWORD', 'rfgsnfyxer86');
+define('DB_DATABASE', 'perekhrestcerami_db');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
